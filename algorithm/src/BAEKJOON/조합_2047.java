72cdf49c589ed8f31c308d77038ebedcd26609cb
byounghoon <passion.mbh@gmail.com>
2018-10-22 17:15:22 +0900

    완료.

--- algorithm/src/BAEKJOON/조합_2047.java
+++ algorithm/src/BAEKJOON/조합_2047.java
@@ -1,38 +1,46 @@
 package BAEKJOON;
 
-import java.io.FileInputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class 조합_2047 {
-	static int cnt, n, k, d[][];
+	static int n, k;
 
 	public static void main(String[] args) throws Exception {
-//		System.setIn(new FileInputStream("input.txt"));
-		Scanner sc = new Scanner(System.in);
+		// System.setIn(new FileInputStream("input.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		cnt = sc.nextInt();
+		
+		// n C 0 = 1
+		// n C n = 1
+		// n C r = n-1 C r + n-1 C r-1
+		// 초기 값으로 1 C 0 = 1 C 1 = 1로 설정
 
-		for (int i = 1; i <= cnt; i++) {
-			n = sc.nextInt();
-			k = sc.nextInt();
-			d = new int[n + 1][k + 1];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		BigInteger d[][];
+		n = Integer.valueOf(st.nextToken());
+		k = Integer.valueOf(st.nextToken());
+		
+		d = new BigInteger[101][101];
+		
+		d[1][0] = BigInteger.ONE;
+		d[1][1] = BigInteger.ONE;
 
-			for (int a = 1; a <= n; a++) {
-				for (int b = 1; b <= k && b <= a; b++) {
-					if (a == b)
-						d[a][b] = 1;
-					else if (b == 1)
-						d[a][b] = a;
-					else if (d[a][b] == 0)
-						d[a][b] = d[a - 1][b - 1] + d[a - 1][b];
-				}
+		for (int a = 1; a <= n; a++) {
+			d[a][0] = BigInteger.ONE;
+			for (int b = 1; b <= a; b++) {
+				if(a==b)
+					d[a][b] = new BigInteger("1");
+				else if (b == 1)
+					d[a][b] = new BigInteger(String.valueOf(a));
+				else
+					d[a][b] = d[a - 1][b - 1].add(d[a - 1][b]);
 			}
-
-			System.out.println(d[n][k]);
-			/*
-			 * for (int a=1; a<=n; a++) { for (int b=1; b<=k; b++) {
-			 * System.out.print(d[a][b] + " "); } System.out.println(""); }
-			 */
 		}
+		System.out.println(d[n][k].toString());
 	}
 }
