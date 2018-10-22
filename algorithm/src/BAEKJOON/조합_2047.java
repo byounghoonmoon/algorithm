da9408ed13349a5ad914710b8b9aeb23f43283e5
byounghoon <passion.mbh@gmail.com>
2018-10-22 15:28:54 +0900

    solution 1st.

--- algorithm/src/BAEKJOON/조합_2047.java
+++ algorithm/src/BAEKJOON/조합_2047.java
@@ -1,5 +1,38 @@
 package BAEKJOON;
 
-public class 조합_2047 {
+import java.io.FileInputStream;
+import java.util.Scanner;
 
+public class 조합_2047 {
+	static int cnt, n, k, d[][];
+
+	public static void main(String[] args) throws Exception {
+//		System.setIn(new FileInputStream("input.txt"));
+		Scanner sc = new Scanner(System.in);
+
+		cnt = sc.nextInt();
+
+		for (int i = 1; i <= cnt; i++) {
+			n = sc.nextInt();
+			k = sc.nextInt();
+			d = new int[n + 1][k + 1];
+
+			for (int a = 1; a <= n; a++) {
+				for (int b = 1; b <= k && b <= a; b++) {
+					if (a == b)
+						d[a][b] = 1;
+					else if (b == 1)
+						d[a][b] = a;
+					else if (d[a][b] == 0)
+						d[a][b] = d[a - 1][b - 1] + d[a - 1][b];
+				}
+			}
+
+			System.out.println(d[n][k]);
+			/*
+			 * for (int a=1; a<=n; a++) { for (int b=1; b<=k; b++) {
+			 * System.out.print(d[a][b] + " "); } System.out.println(""); }
+			 */
+		}
+	}
 }
